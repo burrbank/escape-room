538d3c203c32169c0447fceaec19c4c0ba812bcd
Burrbank <rhea.j.pendragon@gmail.com>
2025-10-18 18:22:12 -0300

    change engine to get mongo uri from environ

--- escape_room/models.py
+++ escape_room/models.py
@@ -1,7 +1,13 @@
+from os import stat, environ
 from typing import ClassVar, Dict
 from pyodmongo import DbEngine, DbModel, Id, Field
 
-engine = DbEngine(mongo_uri="mongodb://root:root@localhost:27017", db_name='escape_room')
+db_user = environ.get("DB_USER", 'admin')
+db_pass = environ.get("DB_PASS", 'admin')
+db_address = environ.get("DB_ADDRESS", 'localhost')
+db_port = environ.get("DB_PORT", "27017")
+")
+engine = DbEngine(mongo_uri=f"mongodb://{db_user}:{db_pass}@{db_address}:{db_port}", db_name='escape_room')
 
 class User(DbModel):
     username: str = Field(index = True, unique = True, default="")
@@ -9,6 +15,10 @@ class User(DbModel):
     inventory: Dict["Item| Id", int] = Field(default_factory=dict)
     location: 'Room | Id'
     _collection: ClassVar = "users"
+
+    @staticmethod
+    def get_user_by_id(id:str):
+        pass
 
 class Room(DbModel):
     name:str = Field(index=True, unique=True)
